9ec9d665908fec2c889d7964defbd37d213f58bf
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-07-24 17:12:02 +0600

    Add files via upload

--- Movie.java
+++ Movie.java
@@ -1,76 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+// Define the Movie class
+public class Movie {
 
-package com.mycompany.movie;
 
-public class Movie 
+// Private instance variables
+private String title;
+private String director;
+private int duration; // duration in minutes
+
+// Getter for title
+public String getTitle() {
+    return title;
+}
+
+// Setter for title
+public void setTitle(String title) {
+    this.title = title;
+}
+
+// Getter for director
+public String getDirector() {
+    return director;
+}
+
+// Setter for director
+public void setDirector(String director) 
 {
-    String Name, Designation ; 
-    int age , salary ;  
-    
-    
-    
-    static String company_name = "XYZ" ; 
-    static String company_address =  "DHAKA" ; 
-    
-    
-    static int totalnumberofemployee  ; 
-    
-    
-    public Movie (int age , int salary, String Name, String Designation ) 
-    {
+    this.director = director;
+}
 
-          this.age = age ; 
-          this.salary = salary ; 
-          this.Name = Name ; 
-          this.Designation = Designation ; 
-          
-          
-          totalnumberofemployee++ ; 
-          
-          
-          
-    
-    }
+// Getter for duration
+public int getDuration() {
+    return duration;
+}
 
-     
-    void display()
-    {
-        System.out.println ("Name : " + Name) ; 
-        System.out.println ("Salary : " + salary) ; 
-        System.out.println ("Age : " + age) ; 
-        System.out.println ("Designation : " + Designation ) ;
-        
-        System.out.println ("Company_name : " +  company_name ) ; 
-        
-        System.out.println ("Company_address : " +  company_address ) ; 
-        
-        
-        
-        
-        
-        
-    }
-    
-    
-    
-    static void Total_employees() 
-    {
-        System.out.println("Total Employees: " + totalnumberofemployee);
-    }
-  
-    
-  
-        
+// Setter for duration
+public void setDuration(int duration) {
+    this.duration = duration;
+}
+
+// Method to get movie details
+public String getMovieDetails() {
+    return "\nTitle: " + title
+            + "\nDirector: " + director
+            + "\nDuration: " + duration + " minutes\n";
+            
+}
+
+// Main method
+public static void main(String[] args) {
+
+    // Create a new Movie object
+    Movie movie = new Movie();
+
+    // Set movie information
+    movie.setTitle("Arrival");
+    movie.setDirector("Christopher Nolan");
+    movie.setDuration(146);
+
+    // Display movie details
+    System.out.println(movie.getMovieDetails());
 }
 
 
-
-
-
-
- 
-
-
-
+}
